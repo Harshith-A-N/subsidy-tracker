ad624f1c2a7800637895f3e916d320d34263e89a
Harshith-A-N <harshithjune10@gmail.com>
2026-08-09 23:25:45 +0530

    Implement compliance milestone tracking

--- src/main/java/com/subsidytracker/disbursement/repository/DisbursementMilestoneRepository.java
+++ src/main/java/com/subsidytracker/disbursement/repository/DisbursementMilestoneRepository.java
@@ -3,12 +3,10 @@ package com.subsidytracker.disbursement.repository;
 import com.subsidytracker.common.enums.ComplianceStatus;
 import com.subsidytracker.disbursement.entity.DisbursementMilestone;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository
 public interface DisbursementMilestoneRepository
         extends JpaRepository<DisbursementMilestone, Long> {
 
@@ -21,8 +19,4 @@ public interface DisbursementMilestoneRepository
     List<DisbursementMilestone> findByComplianceStatusAndDueDateBefore(
             ComplianceStatus status,
             LocalDate date);
-
-    boolean existsByApplicationIdAndSequenceOrder(
-            Long applicationId,
-            int sequenceOrder);
 }
